3596b62c8628a98a5d67b2d9eae4862b854b81af
JavierBellver <elmeta271@hotmail.com>
2017-05-24 14:17:13 +0200

    Fixed bug with token verification

--- server/jwtmanagement.go
+++ server/jwtmanagement.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 	"time"
@@ -16,7 +17,7 @@ func generateToken(username string) string {
 	claims := token.Claims.(jwt.MapClaims)
 
 	claims["username"] = username
-	claims["exp"] = time.Now().Add(time.Hour * 24)
+	claims["exp"] = time.Now().Add(time.Minute * 1)
 
 	tokenString, err := token.SignedString(mySignUpKey)
 
@@ -32,11 +33,16 @@ func generateToken(username string) string {
 func validateToken(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		token, err := jwt.Parse(strings.Split(r.Header.Get("Authorization"), " ")[1], func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Algoritmo de firma invalido")
+			}
+
 			return []byte(mySignUpKey), nil
 		})
 
-		//claims := token.Claims
+		claims := token.Claims.(jwt.MapClaims)
 
+		fmt.Println(claims["exp"])
 		currentUsername = getSessionUsername(token.Raw)
 		if err == nil && token.Valid {
 			next.ServeHTTP(w, r)
